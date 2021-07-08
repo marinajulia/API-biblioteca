93ff14d7fa1f4e6e577540d373716f52d1ad1a37
marinajulia <marinajulia707@gmail.com>
2021-07-08 17:49:50 -0300

    update databse

---
+++ Biblioteca.Infra/Migrations/20210708204758_init.cs
@@ -0,0 +1,258 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Biblioteca.Infra.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autor",
+                columns: table => new
+                {
+                    AutorId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeAutor = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autor", x => x.AutorId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Categoria",
+                columns: table => new
+                {
+                    CategoriaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeCategoria = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    DescriçãoCategoria = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categoria", x => x.CategoriaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Editora",
+                columns: table => new
+                {
+                    EditoraId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeEditora = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Editora", x => x.EditoraId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PerfilUsuario",
+                columns: table => new
+                {
+                    PerfilUsuarioId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Perfil = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PerfilUsuario", x => x.PerfilUsuarioId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StatusLivro",
+                columns: table => new
+                {
+                    StatusLivroId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeStatus = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StatusLivro", x => x.StatusLivroId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StatusUsuario",
+                columns: table => new
+                {
+                    StatusUsuarioId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeStatus = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StatusUsuario", x => x.StatusUsuarioId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Livro",
+                columns: table => new
+                {
+                    LivroId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Titulo = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    ISBN = table.Column<string>(type: "char(13)", nullable: true),
+                    CategoriaId = table.Column<int>(type: "int", nullable: false),
+                    AutorId = table.Column<int>(type: "int", nullable: false),
+                    Descrição = table.Column<string>(type: "nvarchar(1000)", maxLength: 1000, nullable: true),
+                    EditoraId = table.Column<int>(type: "int", nullable: false),
+                    StatusLivroId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livro", x => x.LivroId);
+                    table.ForeignKey(
+                        name: "FK_Livro_Autor_AutorId",
+                        column: x => x.AutorId,
+                        principalTable: "Autor",
+                        principalColumn: "AutorId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Livro_Categoria_CategoriaId",
+                        column: x => x.CategoriaId,
+                        principalTable: "Categoria",
+                        principalColumn: "CategoriaId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Livro_Editora_EditoraId",
+                        column: x => x.EditoraId,
+                        principalTable: "Editora",
+                        principalColumn: "EditoraId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Livro_StatusLivro_StatusLivroId",
+                        column: x => x.StatusLivroId,
+                        principalTable: "StatusLivro",
+                        principalColumn: "StatusLivroId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    UsuarioId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeUsuario = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    CPF = table.Column<string>(type: "char(11)", nullable: true),
+                    Senha = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StatusUsuarioId = table.Column<int>(type: "int", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PerfilUsuarioId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.UsuarioId);
+                    table.ForeignKey(
+                        name: "FK_Usuario_PerfilUsuario_PerfilUsuarioId",
+                        column: x => x.PerfilUsuarioId,
+                        principalTable: "PerfilUsuario",
+                        principalColumn: "PerfilUsuarioId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Usuario_StatusUsuario_StatusUsuarioId",
+                        column: x => x.StatusUsuarioId,
+                        principalTable: "StatusUsuario",
+                        principalColumn: "StatusUsuarioId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UsuarioLivros",
+                columns: table => new
+                {
+                    UsuarioLivrosId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false),
+                    LivroId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UsuarioLivros", x => x.UsuarioLivrosId);
+                    table.ForeignKey(
+                        name: "FK_UsuarioLivros_Livro_LivroId",
+                        column: x => x.LivroId,
+                        principalTable: "Livro",
+                        principalColumn: "LivroId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_UsuarioLivros_Usuario_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "Usuario",
+                        principalColumn: "UsuarioId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livro_AutorId",
+                table: "Livro",
+                column: "AutorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livro_CategoriaId",
+                table: "Livro",
+                column: "CategoriaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livro_EditoraId",
+                table: "Livro",
+                column: "EditoraId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livro_StatusLivroId",
+                table: "Livro",
+                column: "StatusLivroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuario_PerfilUsuarioId",
+                table: "Usuario",
+                column: "PerfilUsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuario_StatusUsuarioId",
+                table: "Usuario",
+                column: "StatusUsuarioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsuarioLivros_LivroId",
+                table: "UsuarioLivros",
+                column: "LivroId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UsuarioLivros_UsuarioId",
+                table: "UsuarioLivros",
+                column: "UsuarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UsuarioLivros");
+
+            migrationBuilder.DropTable(
+                name: "Livro");
+
+            migrationBuilder.DropTable(
+                name: "Usuario");
+
+            migrationBuilder.DropTable(
+                name: "Autor");
+
+            migrationBuilder.DropTable(
+                name: "Categoria");
+
+            migrationBuilder.DropTable(
+                name: "Editora");
+
+            migrationBuilder.DropTable(
+                name: "StatusLivro");
+
+            migrationBuilder.DropTable(
+                name: "PerfilUsuario");
+
+            migrationBuilder.DropTable(
+                name: "StatusUsuario");
+        }
+    }
+}
