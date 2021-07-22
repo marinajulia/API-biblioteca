4847ea6487eca01dc9dbaad98b0fe88ced5fa4e6
Marina Julia <marinajulia707@gmail.com>
2021-07-22 15:32:26 -0300

    verificações na inserção do livro

--- Biblioteca.Domain/Services/Livro/LivroService.cs
+++ Biblioteca.Domain/Services/Livro/LivroService.cs
@@ -1,5 +1,9 @@
-﻿using Biblioteca.Domain.Services.Entidades;
+﻿using Biblioteca.Domain.Services.Autor;
+using Biblioteca.Domain.Services.Categoria;
+using Biblioteca.Domain.Services.Editora;
+using Biblioteca.Domain.Services.Entidades;
 using Biblioteca.Domain.Services.Livro.Dto;
+using Biblioteca.Domain.Services.StatusLivro.Entities;
 using Biblioteca.SharedKernel;
 using SharedKernel.Domain.Notification;
 using System.Collections.Generic;
@@ -12,15 +16,28 @@ namespace Biblioteca.Domain.Services.Livro
         private readonly INotification _notification;
         private readonly ILivroRepository _livroRepository;
         private readonly UserLoggedData _userLoggedData;
+        private readonly ICategoriaRepository _categoriaRepository;
+        private readonly IEditoraRepository _editoraRepository;
+        private readonly IAutorRepository _autorRepository;
+        private readonly IStatusLivroRepository _statusLivroRepository;
 
         public LivroService(
-            ILivroRepository livroRepository, 
-            INotification notification, 
-            UserLoggedData userLoggedData)
+            ILivroRepository livroRepository,
+            INotification notification,
+            UserLoggedData userLoggedData,
+            ICategoriaRepository categoriaRepository,
+            IEditoraRepository editoraRepository,
+            IAutorRepository autorRepository,
+            IStatusLivroRepository statusLivroRepository
+            )
         {
             _livroRepository = livroRepository;
             _notification = notification;
             _userLoggedData = userLoggedData;
+            _categoriaRepository = categoriaRepository;
+            _editoraRepository = editoraRepository;
+            _autorRepository = autorRepository;
+            _statusLivroRepository = statusLivroRepository;
         }
 
         public IEnumerable<LivroDto> Get()
@@ -65,37 +82,58 @@ namespace Biblioteca.Domain.Services.Livro
         {
             var dadosUsuarioLogado = _userLoggedData.GetData();
 
-            if(dadosUsuarioLogado.Id_PerfilUsuario == 1)
+            if (dadosUsuarioLogado.Id_PerfilUsuario == 1)
                 return _notification.AddWithReturn<LivroDto>
                     ("Ops.. parece que você não tem permissão para adicionar este livro");
 
-            else {
-                var livroData = _livroRepository.GetByName(livro.Titulo);
-                if (livroData != null)
-                    return _notification.AddWithReturn<LivroDto>
-                        ("Ops.. parece que esse livro já existe!");
+            var livroData = _livroRepository.GetByName(livro.Titulo);
+            if (livroData != null)
+                return _notification.AddWithReturn<LivroDto>
+                    ("Ops.. parece que esse livro já existe!");
 
-                var livroEntity = _livroRepository.Post(new LivroEntity {
-                    Titulo = livro.Titulo,
-                    ISBN = livro.ISBN,
-                    CategoriaId = livro.CategoriaId,
-                    AutorId = livro.AutorId,
-                    Descrição = livro.Descrição,
-                    EditoraId = livro.EditoraId,
-                    StatusLivroId = livro.StatusLivroId
-                });
+            var verificaSeCategoriaExiste = _categoriaRepository.GetById(livro.CategoriaId);
+            if (verificaSeCategoriaExiste == null)
+                return _notification.AddWithReturn<LivroDto>
+                    ("Ops.. parece que a categoria informada não existe");
 
-                return new LivroDto {
-                    LivroId = livroEntity.LivroId,
-                    Titulo = livroEntity.Titulo,
-                    ISBN = livroEntity.ISBN,
-                    CategoriaId = livroEntity.CategoriaId,
-                    AutorId = livroEntity.AutorId,
-                    Descrição = livroEntity.Descrição,
-                    EditoraId = livroEntity.EditoraId,
-                    StatusLivroId = livroEntity.StatusLivroId
-                };
-            }
+            var verificaSeEditoraExiste = _editoraRepository.GetById(livro.EditoraId);
+            if (verificaSeEditoraExiste == null)
+                return _notification.AddWithReturn<LivroDto>
+                    ("Ops.. parece que a editora informada não existe");
+
+            var verificaSeAutorExiste = _autorRepository.GetById(livro.AutorId);
+            if (verificaSeAutorExiste == null)
+                return _notification.AddWithReturn<LivroDto>
+                    ("Ops.. parece que o autor informado não existe");
+
+            var verificaSeStatusLivroExiste = _statusLivroRepository.GetById(livro.StatusLivroId);
+            if (verificaSeStatusLivroExiste == null)
+                return _notification.AddWithReturn<LivroDto>
+                    ("Ops.. parece que o status do livro informado não existe");
+
+            var livroEntity = _livroRepository.Post(new LivroEntity
+            {
+                Titulo = livro.Titulo,
+                ISBN = livro.ISBN,
+                CategoriaId = livro.CategoriaId,
+                AutorId = livro.AutorId,
+                Descrição = livro.Descrição,
+                EditoraId = livro.EditoraId,
+                StatusLivroId = livro.StatusLivroId
+            });
+
+            return new LivroDto
+            {
+                LivroId = livroEntity.LivroId,
+                Titulo = livroEntity.Titulo,
+                ISBN = livroEntity.ISBN,
+                CategoriaId = livroEntity.CategoriaId,
+                AutorId = livroEntity.AutorId,
+                Descrição = livroEntity.Descrição,
+                EditoraId = livroEntity.EditoraId,
+                StatusLivroId = livroEntity.StatusLivroId
+            };
+
         }
     }
 }
