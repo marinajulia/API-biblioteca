d4e5d293538cc4edda91f80d0c4886ede9931edd
marinajulia <marinajulia707@gmail.com>
2021-08-23 11:18:23 -0300

    tests categoria repository

---
+++ Fixtures/Categoria/CategoriaFixturesEntity.cs
@@ -0,0 +1,56 @@
+﻿using Biblioteca.Domain.Services.Entidades;
+using System;
+using System.Collections.Generic;
+
+namespace Fixtures.Categoria
+{
+    public static class CategoriaFixtureEntity
+    {
+        public static CategoriaEntity CreateValidCategoria()
+        {
+            return new CategoriaEntity
+            {
+                CategoriaId = 1,
+                NomeCategoria = "Romance",
+                DescriçãoCategoria = "teste"
+            };
+        }
+
+        public static CategoriaEntity CreateValidCategoria(string nomeCategoria, string descricaoCategoria)
+        {
+            var random = new Random();
+
+            return new CategoriaEntity
+            {
+                CategoriaId = random.Next(1, 10),
+                NomeCategoria = nomeCategoria,
+                DescriçãoCategoria = descricaoCategoria
+            };
+        }
+        public static CategoriaEntity CreateInvalidCategoria()
+        {
+            return new CategoriaEntity
+            {
+                CategoriaId = 0,
+                NomeCategoria = "",
+                DescriçãoCategoria = "",
+            };
+        }
+
+
+        public static IEnumerable<CategoriaEntity> CreateListValidCategorias()
+        {
+            var categorias = new List<CategoriaEntity>();
+
+            categorias.Add(CreateValidCategoria("Terror", "teste"));
+            categorias.Add(CreateValidCategoria("Suspense", "teste"));
+            categorias.Add(CreateValidCategoria("Comedia", "teste"));
+            categorias.Add(CreateValidCategoria("Aventura", "teste"));
+            categorias.Add(CreateValidCategoria("Drama", "teste"));
+            categorias.Add(CreateValidCategoria("Comedia Romantica", "teste"));
+
+
+            return categorias;
+        }
+    }
+}
--- Infra.Tests/Repositories/Categoria/CategoriaRepositoryTest.cs
+++ Infra.Tests/Repositories/Categoria/CategoriaRepositoryTest.cs
@@ -1,6 +1,51 @@
-﻿namespace Infra.Tests.Repositories.Categoria
+﻿using Biblioteca.Domain.Services.Categoria;
+using Biblioteca.Domain.Services.Entidades;
+using Fixtures.Categoria;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace Infra.Tests.Repositories.Categoria
 {
     public class CategoriaRepositoryTest
     {
+        private readonly Mock<ICategoriaRepository> categoriaRepository = new Mock<ICategoriaRepository>();
+
+        [Fact(DisplayName = "CategoriaRepository, create category should return success")]
+        [Trait("Infra", "Categoria Repository")]
+        public void CategoriaRepository_CreateCategoria_MustReturnSuccess()
+        {
+            var categoria = CategoriaFixtureEntity.CreateValidCategoria();
+
+            categoriaRepository.Setup(x => x.Post(categoria)).Returns(categoria);
+
+            var actualResult = categoriaRepository.Object.Post(categoria);
+
+            Assert.Equal(categoria, actualResult);
+
+            categoria.Should().BeEquivalentTo(actualResult);
+            categoriaRepository.Verify(r => r.Post(It.IsAny<CategoriaEntity>()), Times.Once);
+        }
+
+        [Fact(DisplayName = "CategoriaRepository, create category should return error")]
+        [Trait("Infra", "Categoria Repository")]
+        public void CategoriaRepository_CreateCategoria_MustReturnError()
+        {
+            //Arrange
+            var categoriaInvalida = CategoriaFixtureEntity.CreateInvalidCategoria();
+
+            //var expectedUser = 1;
+
+            categoriaRepository.Setup(x => x.Post(categoriaInvalida)).Returns(categoriaInvalida);
+
+            //Act
+            var actualResult = categoriaRepository.Object.Post(categoriaInvalida);
+
+            //Assert
+            Assert.False(categoriaInvalida.CategoriaId > 0);
+            Assert.False(!string.IsNullOrEmpty(categoriaInvalida.NomeCategoria));
+            categoriaRepository.Verify(r => r.Post(It.IsAny<CategoriaEntity>()), Times.Once);
+        }
+
     }
 }
